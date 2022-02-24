787a858646af669ff3056875a487db0d5acd29ea
MolanZ <mz9kk@mail.umck.edu>
2022-02-24 12:48:54 -0600

    changed file location
    i moved some files following your instruction and if anything need to be modified, please let me know.

--- global_floods_django_app/views.py
+++ global_floods_django_app/views.py
@@ -1,45 +1,82 @@
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-from airavata_django_portal_sdk import user_storage
-
 # Create your views here.
+# coding:utf-8
+from django.http import HttpResponse
+from django.template import loader,Context
+from django.views.generic import TemplateView
+from MainPage.read_MoM_point import *
+import folium
+import datetime
 
-@login_required
-def home(request):
+s_date=''
+e_date=''
 
-    # Example code: Airavata API client
-    # Make calls to the Airavata API from your view, for example:
-    #
-    # experiments = request.airavata_client.searchExperiments(
-    #        request.authz_token, settings.GATEWAY_ID, request.user.username, filters={},
-    #        limit=20, offset=0)
-    #
-    # The authorization token is always the first argument of Airavata API calls
-    # and is available as 'request.authz_token'. Some API methods require a
-    # 'gatewayID' argument and that is available on the Django settings object
-    # as 'settings.GATEWAY_ID'.
-    # For documentation on other Airavata API methods, see
-    # https://docs.airavata.org/en/master/technical-documentation/airavata-api/.
-    # The Airavata Django Portal uses the Airavata Python Client SDK:
-    # https://github.com/apache/airavata/tree/master/airavata-api/airavata-client-sdks/airavata-python-sdk
+def index(request):
+    context = {'map':'load failed'}
+    map = init_map()
+    m = map._repr_html_()
+    context = {'map': m}
+    return render(request, 'index.html', context)
 
+def pick(request):
+    context = {'map':'load failed'}
+    if request.method == "POST":
+        print('xxx')
+        if 'area' in request.POST:
+            print('xxx')
+            value = request.POST.get("myInput", None)
+            res = value.split('Alert level')[0]
+            res = int(res)
+            map = pick_map(res)
+            m = map._repr_html_()
+    else:
+        map = pick_map()
+        m = map._repr_html_()
+        context = {'map': m}
+    return render(request, 'pick.html', context)
 
-    # Example code: user_storage module
-    # In your Django views, you can make calls to the user_storage module to manage a user's files in the gateway
-    #
-    # user_storage.listdir(request, "")  # lists the user's home directory
-    # user_storage.open_file(request, data_product_uri=...)  # open's a file for a given data_product_uri
-    # user_storage.save(request, "path/in/user/storage", file)  # save a file to a path in the user's storage
-    #
-    # For more information as well as other user_storage functions, see https://airavata-django-portal-sdk.readthedocs.io/en/latest/
+def draw(request):
+    context = {'map':'load failed'}
+    map = draw_map()
+    m = map._repr_html_()
+    context = {'map': m}
+    return render(request, 'draw.html', context)
 
-    return render(request, "global_floods_django_app/home.html", {
-        'project_name': "Global Floods Django App"
-    })
+def upload(request):
+    context = {'map':'null'}
+    if request.method == "POST":
+        if 'ufile' in request.POST:
+            File = request.FILES.get("myfile", None)
+            if File is None:
+                return HttpResponse("There is no chosen file ")
+            else:
+                with open("./temp_file/footprint.geojson", 'wb+') as f:
+                    for chunk in  File.chunks():
+                        f.write(chunk)
+            map = show_map()
+            m = map._repr_html_()
+            context = {'map': m}
+            return render(request, 'upload.html', context)
+        elif 'run' in request.POST:
+            s_date = request.POST.get("pre-event")
+            e_date = request.POST.get("post-event")
+            s_date = s_date.replace('-','')
+            e_date = e_date.replace('-','')
+            return render(request, 'result.html')
+    return render(request, 'upload.html')
 
-@login_required
-def mapview(request):
-    return render(request, "global_floods_django_app/map.html")
+def result(request):
+    context = {'map':'Processing'}
+    if request.method == "POST":
+        if 'no' in request.POST:
+            return render(request, 'index.html')
+        else:
+            s_date = request.POST.get("pre-event")
+            e_date = request.POST.get("post-event")
+            s_date = s_date.replace('-','')
+            e_date = e_date.replace('-','')
+            map = pred()
+            m = map._repr_html_()
+            context = {'map': m}
+    return render(request, 'result.html', context)
